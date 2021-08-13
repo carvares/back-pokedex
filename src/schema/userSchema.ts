acb5a1d02a53bdbd035344b934271f4646f065c3
leleo <celso@respondeai.com.br>
2021-08-13 15:28:23 -0300

    Preparando revisão de código

--- src/schema/userSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const userValidation = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  confirmPassword: Joi.string().min(6).required(),
-});
-
-export { userValidation };
--- tests/utils/database.ts
+++
@@ -1,8 +0,0 @@
-import { getRepository } from "typeorm";
-import Session from "../../src/entities/Sessions";
-import User from "../../src/entities/User";
-
-export async function clearDatabase () {
-  await getRepository(Session).delete({});
-  await getRepository(User).delete({});
-}
